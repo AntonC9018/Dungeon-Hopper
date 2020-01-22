9b41dec71feb4bd8ddfbdf15e26345d2703ef4b9
AntonC9018 <anton.curm@gmail.com>
2020-01-22 18:57:08 +0200

    skipped .amount again

--- logic/hp/hp.lua
+++ logic/hp/hp.lua
@@ -7,7 +7,7 @@ local HEART_SIZE = 2
 -- for now I'll keep it simple. health is a number
 function HP:__construct(health)
     self.containers = {}
-    
+
     local i = health
 
     while i > 0 do
@@ -58,7 +58,7 @@ end
 function HP:get()
     local health = 0
     for i = 1, #self.containers do
-        health = health + self.containers[i]
+        health = health + self.containers[i].amount
     end
     return health
 end

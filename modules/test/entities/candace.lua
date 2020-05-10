89b17962d3692d0c1cccb656dc31f3cfe39c21ac
AntonC9018 <anton.curm@gmail.com>
2020-05-10 19:56:06 +0300

    candace

---
+++ modules/test/entities/candace.lua
@@ -0,0 +1,20 @@
+local EnvObject = require 'modules.test.base.envobject'
+local Entity = require 'logic.base.entity'
+local Player = require 'logic.base.player'
+local Pierce = require 'modules.test.retouchers.pierce'
+
+local Candace = class('Candace', Player)
+
+-- Entity.copyChains(EnvObject, Joe)
+
+Candace.baseModifiers = {
+    hp = {
+        amount = 100
+    },
+    attack = {
+        damage = 1,
+        pierce = 2        
+    }
+}
+
+return Candace
